f92ed1f2ba4b9a9bfaa4d46c4ea3989a8ed87a1f
Laura Hackney <laura@anniecannons.com>
2023-08-05 10:33:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Functions and Scope in JavaScript
+
+## Scope
+
+- In JavaScript, scope refers to the context in which variables are declared and accessed. It defines the visibility and accessibility of variables in different parts of your code during runtime. Understanding scope is crucial for writing bug-free, maintainable, and efficient JavaScript code.
+
+- Global Scope: Variables declared outside of any function or block have global scope. They are accessible from anywhere in the code, including other functions and blocks. 
+
+ ```javascript
+const globalVar = 42; // Global variable
+
+function doSomething() {
+  console.log(globalVar); // Accessible inside the function
+}
+
+console.log(globalVar); // Accessible outside the function
+ ```
+
+ - Local Scope: Variables declared inside a function or block have local scope. They are only accessible within the function or block in which they are defined. Local variables are created when the function or block is called and destroyed when it exits, freeing up memory.
+
+ ```javascript
+function doSomething() {
+  const localVar = 10; // Local variable
+  console.log(localVar); // Accessible inside the function
+}
+
+doSomething();
+// console.log(localVar); // Error: localVar is not accessible here
+ ```
+
+
+### Prep
+
+1. Connect your js file to your HTML file
+2. Open your code in the browser and open your console
+
+### Activities
+1. In the `js` file, complete the list of exercises. 
---
+++ scripts.js
@@ -0,0 +1,131 @@
+/********************* Exercise 1  **********************/
+const numberOfUsers = 15;
+
+function authentication() {
+  const numberOfAuthenticatedUsers = 10;
+  console.log(numberOfUsers); 
+  console.log(numberOfAuthenticatedUsers); 
+}
+
+console.log(numberOfUsers); 
+console.log(numberOfAuthenticatedUsers); 
+
+authentication();
+
+// Explain what will be logged in the console for each of the console logs in the block of code above. 
+
+
+
+
+
+
+
+/********************* Exercise 2  **********************/
+
+let baseSalary = 50000;
+
+function calculateBonus() {
+  const bonusPercentage = 0.1; // 10% bonus
+  return baseSalary * bonusPercentage;
+}
+
+function calculateTotalSalary() {
+  const bonus = calculateBonus();
+  return baseSalary + bonus;
+}
+
+// Call the calculateTotalSalary function. Explain below each of the steps that are taken by the functions.
+
+
+// What is the output?
+
+
+
+
+// How would you change the functions so that instead of a global variable the function takes in an argument for the base salary?
+
+
+
+
+
+
+/********************* Exercise 3  **********************/
+
+const user1 = {
+    name: "Johnny Diaz",
+    age: 30,
+    address: {
+      city: "New York",
+      country: "USA",
+    },
+  };
+  
+const user2 = {
+    name: "Lupita Davenport",
+    age: 30,
+    address: {
+        city: "London",
+        country: "UK",
+    },
+};
+
+// Create a function that can take in a user variable. Return a string that says, "[NAME] lives in [CITY], [COUNTRY]"
+
+
+
+
+
+
+
+ 
+/********************* Exercise 4  **********************/
+const books = ['Anna Karenina', 'To Kill a Mockingbird', 'Where the Sidewalk Ends', 'The Fellowship of the Ring', 'A Wrinkle in Time'];
+
+
+function createBookCollection(books) {  
+    let removeLastBook = books.pop();
+    return `You have read ${books.length} books!`;
+  }
+
+
+const bookCollection = createBookCollection();
+
+console.log(bookCollection);
+  
+// Run the above code. What error do you see? How would you fix this code?
+
+
+
+
+
+
+/********************* Exercise 5  **********************/
+// Declare a function named calculateDogAge that:
+// Takes 1 argument: your puppy's age.
+// Calculates your dog's age by multiplying the puppy’s age by 7.
+// Returns the result of a string like so: "Your doggie is NN years old in dog years!"
+
+// Call the function three times with different sets of values.
+
+
+
+
+
+
+// What type of scope are you using to declare the variables? 
+
+
+
+
+
+/********************* Exercise 6  **********************/
+var userName = 'Katherine';
+
+function showMessage(userName) {
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+
+showMessage('Dominique');
+
+// What would print in the alert message when the following code is executed?
